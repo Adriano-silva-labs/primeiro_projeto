a5a12c70b1b6abec3edbb919d0874f3c8995f773
Adriano Silva <adrianooliveira709@gmail.com>
2024-08-09 20:02:33 -0300

    mudei adição para multiplicação

--- exercicio3.py
+++ exercicio3.py
@@ -1,6 +1,6 @@
-print( 'Vamos somar!')
+print( 'Vamos multiplicar!')
 n1 = int(input ('Digite um número'))
 n2 = int(input('Digite outro número'))
-s = n1 + n2
+s = n1 * n2
 print ('O resultado é:', s)
 
